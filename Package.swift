96d7c1a294d1f5af398762e32e7b7f1638e3e67d
Andrew Liu <Admin@Admins-MacBook-Pro.local>
2017-02-02 17:44:24 -0800

    add postgresql-provider package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "hello-vapor-world",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4),
+         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
     ],
     exclude: [
         "Config",
