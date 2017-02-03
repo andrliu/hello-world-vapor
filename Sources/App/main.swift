e6921f78513759923b9bbd6ef0d522564436a357
Andrew Liu <Admin@Admins-MacBook-Pro.local>
2017-02-03 11:26:46 -0800

    CRUD

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -102,4 +102,48 @@ drop.get("test") { request in
     return try JSON(node: Acronym.all().makeNode())
 }
 
+//MARK:
+//MARK: CRUD Database Options
+//MARK: Create
+drop.get("new") { request in
+    var acronym = try Acronym(node: request.json)
+    try acronym.save()
+    return acronym
+}
+
+//MARK: Read
+drop.get("all") { request in
+    return try JSON(node: Acronym.all().makeNode())
+}
+
+drop.get("first") { request in
+    return try JSON(node: Acronym.query().first()?.makeNode())
+}
+
+drop.get("afks") { request in
+    return try JSON(node: Acronym.query().filter("short", "AFK").all().makeNode())
+}
+
+drop.get("not-afks") { request in
+    return try JSON(node: Acronym.query().filter("short", .notEquals,"AFK").all().makeNode())
+}
+
+//MARK: Update
+drop.get("update") { request in
+    guard var first = try Acronym.query().first(),
+        let long = request.data["long"]?.string else {
+            throw Abort.badRequest
+    }
+    first.long = long
+    try first.save()
+    return first
+}
+
+//MARK: Delete
+drop.get("delete-afks") { request in
+    let query = try Acronym.query().filter("short", "AFK")
+    try query.delete()
+    return try JSON(node: Acronym.all().makeNode())
+}
+
 drop.run()
